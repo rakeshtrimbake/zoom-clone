f5d64d2ccfaeedfcac5fa9221b7a07f37625bea5
unknown <trimbake.rakesh1@gmail.com>
2020-08-11 21:13:03 +0530

    minor changes

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   secure: true, 
-  host: '/zoom-app-clone-demo.herokuapp.com/',
+  host: 'zoom-app-clone-demo.herokuapp.com/',
   port: 443
   
 })
