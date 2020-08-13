c9cc21ce89669f5e4e7f26b8b326f49f6031ba31
unknown <trimbake.rakesh1@gmail.com>
2020-08-13 19:57:25 +0530

    minor changes

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  path:'peerjs',
+  path:'/peerjs',
   host: '/',
   port: '443'
 })
--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
-
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+app.use('/peerjs', peerServer);
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
